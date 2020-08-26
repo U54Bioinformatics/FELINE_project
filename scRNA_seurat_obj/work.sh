ce414c31acd9bc0be45c073888d4215fd17f0fa5
jichen <jichen@coh.org>
2020-08-25 18:07:40 -0700

    10x seurat obj

---
+++ scRNA_seurat_obj/work.sh
@@ -0,0 +1,5 @@
+
+echo "Final seurat obj with 35 patients with 10x scRNA data"
+cp ../Seurat_integration_10x/FEL011046_10x_Seurat_2kgenes_vst_cc.integrated_RPCA.rds ./
+ln -s FEL011046_10x_Seurat_2kgenes_vst_cc.integrated_RPCA.rds FEL011046_10x_Seurat_2kgenes_vst_cc.rds
+
